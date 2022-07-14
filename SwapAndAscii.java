2e9aafab90f385abd2a35589d592fcac07ba2d5b
Chandru <chandrutallikeri319@gmail.com>
2022-07-15 00:15:50 +0530

    Update SwapAndAscii.java

--- SwapAndAscii.java
+++ SwapAndAscii.java
@@ -1,9 +1,9 @@
-package string;
 import java.util.*;
-public class Demo4 {
+public class SwapAndAscii 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
 
 		Scanner sc = new Scanner(System.in);
 		
